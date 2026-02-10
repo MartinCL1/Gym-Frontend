fe388d6680fbe1972ca9952e41f8d0db831a6b9d
Martin Cox <coxmartin877@gmail.com>
2026-02-09 21:52:03 -0600

    Se anadio el modal y se coloco un toast para que muestre si fue succesfully completed le peticion o no

--- src/features/layout/MemberLayout/MemberLayout.jsx
+++ src/features/layout/MemberLayout/MemberLayout.jsx
@@ -1,12 +1,29 @@
 import "./member.css";
 import CardInformation from "../../../shared/cardInformation/CardInformation";
 import useGet from "../../../hooks/useGet";
-import { AnimatePresence, motion } from "motion/react";
-import React, { useState } from "react";
-import { Upload, X } from "lucide-react";
+import { useEffect, useRef, useState } from "react";
+import { usePost } from "../../../hooks/usePost";
+import { Dialog } from "radix-ui";
+import { AnimatePresence, motion } from 'motion/react'
+import { Upload } from "lucide-react";
+
 
 const MemberLayout = () => {
   const [mostrarModalPublicacion, setMostrarModalPublicacion] = useState(false);
+  const { cargando:postCargando, respuesta, enviarPeticion } = usePost(false);
+  const [visibilidadToast, setVisibilidadToast] = useState(false)
+
+  useEffect(() => {
+    if(postCargando || respuesta) {
+      setVisibilidadToast(true)
+
+      setTimeout(() => {
+        setVisibilidadToast(false)
+      }, 2000);
+    }
+  }, [postCargando, respuesta])
+
+
   const { acceso, cargando, informacion } = useGet(
     "http://localhost:3500/publicaciones",
   );
@@ -32,11 +49,19 @@ const MemberLayout = () => {
       <div className="member-opciones">
         <button onClick={openModal}>Agregar Publicacion</button>
       </div>
+      
       <AnimatePresence>
-        {mostrarModalPublicacion && (
-          <ModalPublicacion cerrarModal={closeModal} />
-        )}
+        {
+          visibilidadToast &&
+          <PopUp cargando={postCargando} respuesta={respuesta} />
+        }
       </AnimatePresence>
+      <ModalPublicacion 
+        visible={mostrarModalPublicacion} 
+        cerrarModal={closeModal} 
+        enviarPeticion={enviarPeticion} 
+      />
+
     </div>
   );
 };
@@ -44,45 +69,86 @@ const MemberLayout = () => {
 // !? Modal para mostrar en la creacion de una nueva publicacion.
 //! Se debe de cambiar por un archivo unico.
 
-const ModalPublicacion = React.memo(({ cerrarModal }) => {
+const ModalPublicacion = ({ visible, cerrarModal, enviarPeticion }) => {
+  const referenciaFile = useRef(null)
+  const [publicacion, setPublicacion] = useState({
+    titulo: "",
+    descripcion: "",
+    imagen: "cualquiera"
+  }) 
 
-  
-  const subirPublicacion = () => {
+  const seleccionarImagen = () => {
+    referenciaFile.current.click()
+  }
+
+  const modificarPublicacion = (ev) => {
+    const nombre = ev.target.name
+    const valor = ev.target.value
+
+    setPublicacion({
+      ...publicacion,
+      [nombre]: valor
+    })
+  }
+
+  const capturarFile = () => {
     
   }
 
+  const subirPublicacion = async () => {
+    // Primero debemos de validar la informacion...
+    cerrarModal()
+    await enviarPeticion('publicaciones', publicacion)
+  };
+
   return (
-    <motion.div
-      className="modal-publicacion flex-center"
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      exit={{ opacity: 0 }}
+    <Dialog.Root open={visible} >
+      <Dialog.Portal>
+        <Dialog.Overlay className="modal-overlay" />
+        <Dialog.Content className="modal-contenido flex-center">
+          <Dialog.Title className="modal-titulo">Crea tu publicacion</Dialog.Title>
+          <div className="contenido-principal">
+            <div className="modal-titulo-publicacion">
+              <input type="text" name="titulo" placeholder="Ingresa el titulo de tu publicacion..." onChange={modificarPublicacion} />
+            </div>
+            <div className="modal-descripcion-publicacion">
+              <textarea name="descripcion" id=""  placeholder="Escribe algo..." onChange={modificarPublicacion}>
+              </textarea>
+            </div>  
+            <div className="modal-imagen-publicacion" onClick={seleccionarImagen}>
+              <Upload  className="upload-imagen"/>
+              <span>Subir imagen</span>
+            </div>
+          </div>
+          <div className="modal-botones flex-center">
+            <button onClick={cerrarModal}>Cancelar</button>
+            <button onClick={subirPublicacion}>Aceptar</button>
+          </div>
+          <input type="file" accept="image/png" onChange={capturarFile} hidden ref={referenciaFile} />
+        </Dialog.Content>
+      </Dialog.Portal>
+    </Dialog.Root>
+  )
+};
+
+
+const PopUp = ({cargando=false, respuesta = true}) => {
+
+  return (
+    <motion.div className="advise flex-center"
+      initial={{right: "-50%"}}
+      animate={{right: "2%"}}
+      exit={{right: "-50%"}}
     >
-      <X className="close-modal" onClick={cerrarModal} />
-
-      <div className="publicacion-informacion flex-center">
-        {/* Contiene el titulo de la publicacion */}
-        <div className="titulo-publicacion">
-          <input type="text"  placeholder="Titulo..." />
-        </div>
-        {/* Contiene descripcion de la publicacion*/}
-        <div className="descripcion-publicacion">
-          <textarea  name="" id="" placeholder="Publica algo...">
-          </textarea>
-        </div>
-        {/* Contiene imagen de la publicacion */}
-        <div className="file-upload flex-center">
-          <Upload className="publicacion-imagen-upload" />
-          <span>Subir archivos</span>
-        </div>
-
-        <div className="opciones-publicacion">
-          <button>Publicar</button>
-          <button>Cancelar</button>
-        </div>
-      </div>
+      {
+        cargando && <span>Cargando...</span>
+      }
+      {
+        (!cargando && respuesta) && <span>Se añadio correctamente!</span>
+      }
+      
     </motion.div>
-  );
-});
+  )
+}
 
 export default MemberLayout;
--- src/hooks/usePost.jsx
+++ src/hooks/usePost.jsx
@@ -1,21 +1,32 @@
 import { useState } from "react"
 
 
-const usePost = () => {
+export const usePost = () => {
     const [cargando, setCargando] = useState(false)
     const [respuesta, setRespuesta] = useState(null)
     const [error, setError] = useState(null)
 
-    const enviarPeticion = () => {
+    const enviarPeticion = async(ruta, datos) => {
         setCargando(false)
 
         try {
-            const peticion = fetch('http://localhost:3500/')
-        }catch {
+            const peticion = await fetch(`http://localhost:3500/${ruta}`, {
+                method: "POST",
+                credentials: "include",
+                body: JSON.stringify(datos),
+                headers: {"Content-Type": "application/json"}
+            })
 
+            const respuestaPeticion = await peticion.json() 
+
+            setCargando(false)
+            setRespuesta(respuestaPeticion)
+        } catch {
+            setCargando(false)
+            setError(true)
         }
 
     }
 
-    return {cargando, respuesta, enviarPeticion}
+    return { cargando, error, respuesta, enviarPeticion }
 }
